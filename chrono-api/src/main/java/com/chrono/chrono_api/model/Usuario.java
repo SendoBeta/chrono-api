870b43187534957383ad78bfc8e975bf61cec1ba
SendoBeta <alarconduartemoises@gmail.com>
2026-06-15 14:40:37 -0500

    Se ajusta nombre de columba de db usuarios

--- chrono-api/src/main/java/com/chrono/chrono_api/model/Usuario.java
+++ chrono-api/src/main/java/com/chrono/chrono_api/model/Usuario.java
@@ -10,6 +10,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private Long id;
 
     @Column(nullable = false, unique = true)
